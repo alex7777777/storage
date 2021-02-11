8eb60f006f56e02be5eee674b3081fe692cb8167
alexgorbach <gorbach@cubematrix.com>
2021-02-11 22:14:09 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Feb 11 21:53:07 2021
+ASSIGNING VARIABLES
+@author: Alexander Gorbach
+"""
+
+import pandas as pd
+import datetime as dt
+from datetime import datetime, timedelta
+
+# Time window
+START_DATE   = '2015-04-01'
+END_DATE     = str(pd.Timestamp.now() - pd.Timedelta('1 days'))[:10] # yesterday
+# END_DATE     = '2020-12-31'
+
+YEARS_HISTORICAL_DATA = ((pd.to_datetime(END_DATE, format='%Y-%m-%d %H:%M:%S')-
+                          pd.to_datetime(START_DATE, format='%Y-%m-%d %H:%M:%S'))/365).days
+LAST_DAYS             = 21
+FORECAST_DAYS         = 365*5
+BUNDESLAND            = 'NRW'
+SELECTED_CITY         = 'Köln-Bonn'
+
+# Project name & folders
+PRJ_NAME     = 'bike_bonn'
+folders      = [PRJ_NAME, 
+                PRJ_NAME + "/data", 
+                PRJ_NAME + "/data/backup",
+                PRJ_NAME + "/data/covid19",
+                PRJ_NAME + "/data/holidays",
+                PRJ_NAME + "/data/geo", 
+                PRJ_NAME + "/data/platform",
+                PRJ_NAME + "/data/raw_data",
+                PRJ_NAME + "/data/stock",
+                PRJ_NAME + "/data/traffic_jam",
+                PRJ_NAME + "/data/weather", 
+                PRJ_NAME + "/data/weather/temp", 
+                PRJ_NAME + "/doc",
+                PRJ_NAME + "/eda", 
+                PRJ_NAME + "/eda/img",
+                PRJ_NAME + "/model", 
+                PRJ_NAME + "/profiling", 
+                PRJ_NAME + "/result"]
+
+# Data paths
+DATA_PATH      = '/content/drive/My Drive/' + PRJ_NAME + '/data'
+BACKUP_PATH    = '/content/drive/My Drive/' + PRJ_NAME + '/data/backup'
+COVID_PATH     = '/content/drive/My Drive/' + PRJ_NAME + '/data/covid19'
+HOLIDAYS_PATH  = '/content/drive/My Drive/' + PRJ_NAME + '/data/holidays'
+GEO_PATH       = '/content/drive/My Drive/' + PRJ_NAME + '/data/geo'
+RAWDATA_PATH   = '/content/drive/My Drive/' + PRJ_NAME + '/data/raw_data'
+STOCK_PATH     = '/content/drive/My Drive/' + PRJ_NAME + '/data/stock'
+TRAFFIC_PATH   = '/content/drive/My Drive/' + PRJ_NAME + '/data/traffic_jam'
+WEATHER_PATH   = '/content/drive/My Drive/' + PRJ_NAME + '/data/weather'
+WEATHER_T_PATH = '/content/drive/My Drive/' + PRJ_NAME + '/data/weather/temp'
+EDA_PATH       = '/content/drive/My Drive/' + PRJ_NAME + '/eda'
+MODEL_PATH     = '/content/drive/My Drive/' + PRJ_NAME + '/data/model'
+PLATFORM_PATH  = '/content/drive/My Drive/' + PRJ_NAME + '/data/platform'
+PROF_PATH      = '/content/drive/My Drive/' + PRJ_NAME + '/data/profiling'
+RESULT_PATH    = '/content/drive/My Drive/' + PRJ_NAME + '/result'
+
+# Counter selection
+COUNTER        = 100021143 # 'Kennedybrücke (gesamt)'
+
+# MODELING PARAMETER
+YEAR_SEL       = 2019
+YEAR_SEL_WEEK  = 35
+NR_TRAIN_DF    = 10
+
+### ACTIVATE GLOBAL VARIABLES FROM COLAB FILE
+#
+# import sys
+# sys.path.append('/content/drive/My Drive/Colab Notebooks')
+
+# import VARIABLES as gv
+# import FUNCTIONS as gf
+
+# as_str = f'''
+# Project:          %s
+# Time window:      %s - %s
+# Selected Counter: %d
+# BACKUP_PATH:      %s
+# '''
+# print(as_str % (gv.PRJ_NAME, gv.START_DATE, gv.END_DATE, gv.COUNTER, gv.BACKUP_PATH))
